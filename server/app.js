506b2f604f2e6a8308bb50e54c4cf7d43fadd1ba
Jesus Escalante <jeemcars@hotmail.com>
2026-01-13 13:36:00 -0600

    corregida la implemenetacion de la pagina 404

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.get('/generador', (req, res) => {
         .catch(err => console.log(err));
 });
 
-app.get('*', (req, res) => {
+app.use((req, res, next) => {
     res.status(404).send('<h1>404 - Página no encontrada</h1>');
 });
 
